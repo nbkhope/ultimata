af70a5ed9ea3e25c3a0e074aa023638f4011a1f4
nbkhope <ryumemaru@yahoo.com>
2025-12-28 21:44:54 -0500

    Add FontManasger.cpp and FontManager.h

---
+++ src/FontManager.h
@@ -0,0 +1,30 @@
+#ifndef _FONT_H_
+#define _FONT_H_
+
+#include <string>
+#include <map>
+
+#include <SDL/SDL_ttf.h>
+
+using namespace std;
+
+class FontManager
+{
+public:
+	FontManager();
+	~FontManager();
+
+//	string load(string name);
+private:
+
+	struct Font
+	{
+		string name;
+		TTF_Font* font;
+	};
+
+	string directory;
+	map <string, Font> fonts;
+};
+
+#endif
